7b886a4d83cc5ffc9f5e3a6ae0b84e60b665b2ca
shanexuu89 <61563519+shanexuu89@users.noreply.github.com>
2020-08-19 16:51:18 +1200

    issues of port

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 ﻿const http = require('http')
 const fs = require('fs')
-const port = 3000
+const port = 8080
 
 const server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' })
