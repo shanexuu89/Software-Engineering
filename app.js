35a56ed7f8b298993db4c61b692284a5e5ae0375
altria <junchen4933@gmail.com>
2020-09-02 11:25:00 +1200

    try to fix issue with heroku

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
