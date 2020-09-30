b0d89eb9684c9483b6331fcccf7238df5b17962f
shanexuu89 <61563519+shanexuu89@users.noreply.github.com>
2020-10-01 00:57:17 +1300

    update_done

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var signupRouter = require('./routes/signup');
+var submitRouter = require('./routes/submit');
 var app = express();
 
 // view engine setup
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/signup', signupRouter);
+app.use('/submit', submitRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
