b934ab7add7d40b77dec0acc11eb4546b98eac25
shanexuu89 <61563519+shanexuu89@users.noreply.github.com>
2020-10-01 22:14:03 +1300

    signup

--- controller/auth.js
+++ controller/auth.js
@@ -1,4 +1,36 @@
-exports.register = (req,res) => {
+var mysql = require ("mysql");
+const bcrypt = require('bcryptjs');
+var db = mysql.createConnection({
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'bc04aa6c090060',
+    password: '2097f7d0',
+    database: 'heroku_563076973ffff09'
+    });
+exports.login = async (rew, res) => {
+   try {
+       const { email, password} = req.body;
+
+
+   } catch (error){
+       console.log(error);
+   }
+};
+exports.signup = (req,res) => {
     console.log(req.body);
+    const { username, fullname, email, password, affiliation, age, gender } = req.body;
+    /*
+    db.query('SELECT username FROM user WHERE username = ?', [username], (error, results) => {
+        if(error){
+            console.log(error);
+        }
+        if( result.length > 0){
+            return res.render('signup', {
+                message: 'That username is already in use'
+            });
+        }
+     
+    });
+    */ 
+    db.query('INSERT INTO user SET ?', {username: username, fullname: fullname, email: email, password: password, affiliation: affiliation, age: age, gender: gender});
     res.send ("Form submitted");
 }
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 const authController = require ('../controller/auth');
 
-router.post('/register',authController.register);
+router.post('/signup',authController.signup);
+router.post('/login',authController.login);
 
 module.exports = router;
 
